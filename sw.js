87c2cb69e9ea69dd76b79b6ed6d743a6df2fd189
xie633-arch <xie633.633@gmail.com>
2026-09-14 21:43:28 +0800

    chore: cache StudyEvent read model

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'learning-pwa-v0.12.0';
+const CACHE_NAME = 'learning-pwa-v0.12.1';
 const APP_SHELL = [
   './',
   './index.html',
@@ -21,6 +21,7 @@ const APP_SHELL = [
   './learner-data-v1.js',
   './state-write-guard-v113.js',
   './formal-test-adapter-v111.js',
+  './study-event-read-model-v12.js',
   './lesson-content-v05.js',
   './learning-ui-v05.js',
   './phone-knowledge-v09.js',
